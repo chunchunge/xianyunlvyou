f9e1a74e8eaa2ede3ca2cb82150dc955481c8012
qiangyanhuanxiao <5401587+chunchunge@user.noreply.gitee.com>
2019-11-08 15:11:27 +0800

    修改默认

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,14 +5,16 @@ export default {
   ** Headers of the page
   */
   head: {
-    title: process.env.npm_package_name || '',
+    title: "骚气十足的旅游网站",
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', type: 'text/css', href: '//at.alicdn.com/t/font_1168872_ehvuah8v57g.css'} // 新增全局字体样式
+    
     ]
   },
   /*
@@ -50,6 +52,7 @@ export default {
   ** See https://axios.nuxtjs.org/options
   */
   axios: {
+    baseURL: "http://127.0.0.1:1337" // 新增axios默认请求路径 	
   },
   /*
   ** Build configuration
