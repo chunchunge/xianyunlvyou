1ccd9ab6d199b885b79d6b8ea8bce083e1b5a421
qiangyanhuanxiao <5401587+chunchunge@user.noreply.gitee.com>
2019-11-09 19:39:48 +0800

    创建拦截插件

---
+++ plugins/axios.js
@@ -0,0 +1,14 @@
+// 拦截插件
+import {Message} from 'element-ui';
+export default function({$axios,redirect}){
+    $axios.onError(err=>{
+        const {statusCode,message}=err.response.data;
+        // if(statusCode===401||statusCode===403){
+        //     Message.warning({message:"请先登录"});
+        //     redirect("/user/login");
+        // }
+        if(statusCode===400){
+            Message.warning({message});
+        }
+    })
+}
