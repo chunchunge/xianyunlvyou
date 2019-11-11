a0086eb104a1a38673f8f6b5046310ff44f100bb
qiangyanhuanxiao <5401587+chunchunge@user.noreply.gitee.com>
2019-11-11 17:03:56 +0800

    特价机票数据渲染

--- pages/air/index.vue
+++ pages/air/index.vue
@@ -41,6 +41,7 @@
     <div class="air-sale">
       <el-row type="flex" class="air-sale-pic" justify="space-between">
         <el-col :span="6" v-for="(item, index) in sales" :key="index">
+          <!-- 跳转时的参数拼接 -->
           <nuxt-link
             :to="`/air/flights?departCity=${item.departCity}&departCode=${item.departCode}&destCity=${item.destCity}&destCode=${item.destCode}&departDate=${item.departDate}`"
           >
@@ -64,19 +65,16 @@ export default {
   },
   data() {
     return {
-      sales: [
-        {
-          cover:
-            "https://gss0.bdstatic.com/94o3dSag_xI4khGkpoWK1HF6hhy/baike/s%3D220/sign=9154c841bcfd5266a32b3b169b199799/3812b31bb051f8199687c7e0d0b44aed2f73e7fe.jpg",
-          departCity: "广州",
-          departCode: "CAN",
-          departDate: "2019-06-17",
-          destCity: "上海",
-          destCode: "SHA",
-          price: 760
-        }
-      ]
+      sales: []
     };
+  },
+  // 当页面加载完时就发送ajax请求获取数据
+  mounted(){
+    this.$axios({
+      url:'/airs/sale'
+    }).then(res=>{
+      this.sales=res.data.data;
+    })
   }
 };
 </script>
