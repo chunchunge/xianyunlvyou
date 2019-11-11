997a6646163d1291de5f2e949f93b4b86604a74e
qiangyanhuanxiao <5401587+chunchunge@user.noreply.gitee.com>
2019-11-11 19:09:49 +0800

    实现分页

--- pages/air/flights.vue
+++ pages/air/flights.vue
@@ -1,82 +1,111 @@
 <template>
-    <section class="contianer">
-        <el-row  type="flex" justify="space-between">
+  <section class="contianer">
+    <el-row type="flex" justify="space-between">
+      <!-- 顶部过滤列表 -->
+      <div class="flights-content">
+        <!-- 过滤条件 -->
+        <div></div>
 
-            <!-- 顶部过滤列表 -->
-            <div class="flights-content">
-                <!-- 过滤条件 -->
-                <div>
-                    
-                </div>
-                
-                <!-- 航班头部布局 -->
-                 <FlightsListHead/>
-                
-                
-                <!-- 航班信息 -->
-                <div>
-                     <!-- 航班列表 -->
-                    <FlightsItem v-for="(item, index) in dataList" 
-                    :key="index" 
-                    :data="item"/>
-                </div>
-            </div>
+        <!-- 航班头部布局 -->
+        <FlightsListHead />
 
-            <!-- 侧边栏 -->
-            <div class="aside">
-                <!-- 侧边栏组件 -->
-            </div>
-        </el-row>
-    </section>
+        <!-- 航班信息 -->
+        <div>
+          <!-- 航班列表 -->
+          <FlightsItem v-for="(item, index) in dataList" :key="index" :data="item" />
+          <!-- 分页 -->
+          <el-row type="flex" justify="center" style="margin-top:10px;">
+            <!-- size-change：切换条数时候触发 -->
+            <!-- current-change：选择页数时候触发 -->
+            <!-- current-page: 当前页数 -->
+            <!-- page-size：当前条数 -->
+            <!-- total：总条数 -->
+            <el-pagination
+              @size-change="handleSizeChange"
+              @current-change="handleCurrentChange"
+              :current-page="pageIndex"
+              :page-sizes="[5, 10, 15, 20]"
+              :page-size="pageSize"
+              layout="total, sizes, prev, pager, next, jumper"
+              :total="flightsData.total"
+            ></el-pagination>
+          </el-row>
+        </div>
+      </div>
+
+      <!-- 侧边栏 -->
+      <div class="aside">
+        <!-- 侧边栏组件 -->
+      </div>
+    </el-row>
+  </section>
 </template>
 
 <script>
-
 import moment from "moment";
-import FlightsListHead from "@/components/air/flightsListHead.vue"
-import FlightsItem from "@/components/air/flightsItem.vue"
+import FlightsListHead from "@/components/air/flightsListHead.vue";
+import FlightsItem from "@/components/air/flightsItem.vue";
 export default {
-     components: {
-        FlightsListHead,
-          FlightsItem
+  components: {
+    FlightsListHead,
+    FlightsItem
+  },
+  data() {
+    return {
+      flightsData: {}, // 航班总数据
+      dataList: [], // 航班列表数据，用于循环flightsItem组件，单独出来是因为要分页
+      pageIndex: 1, // 当前页数
+      pageSize: 5 // 显示条数
+    };
+  },
+  methods: {
+    // 获取航班总数据
+    getData() {
+      this.$axios({
+        url: "airs",
+        params: this.$route.query //获取来自url上的五个数据
+      }).then(res => {
+        this.flightsData = res.data;
+       this.setDataList(); // 初始化dataList数据，获取1 - 10条
+      })
     },
-    data(){
-        return {
-             flightsData: {}, // 航班总数据
-            dataList: []     // 航班列表数据，用于循环flightsItem组件，单独出来是因为要分页
-        }
+    setDataList(){
+        const start=(this.pageIndex-1)*this.pageSize;
+        const end =start+this.pageSize;
+        this.dataList=this.flightsData.flights.slice(start,end);
     },
-    methods:{
-        // 获取航班总数据
-        getData(){
-            this.$axios({
-                url:'airs',
-                params:this.$route.query//获取来自url上的五个数据
-
-            }).then(res=>{
-                this.flightsData=res.data;
-                this.dataList=this.flightsData.flights;
-            })
-        }
+    // 切换条数时触发的函数
+    handleSizeChange(value){
+        this.pageSize=value;
+        this.pageIndex=1;
+        this.setDataList();
     },
-    mounted(){
-        this.getData();
+    // 切换页数时触发的函数
+    handleCurrentChange(value){
+        this.pageIndex=value;
+        this.setDataList();
     }
-}
+
+  },
+  mounted() {
+    this.getData();
+  },
+
+};
 </script>
 
 <style scoped lang="less">
-    .contianer{
-        width:1000px;
-        margin:20px auto;
-    }
+.contianer {
+  width: 1000px;
+  margin: 20px auto;
+}
 
-    .flights-content{
-        width:745px;
-        font-size:14px;
-    }
+.flights-content {
+  width: 745px;
+  font-size: 14px;
+}
 
-    .aside{
-        width:240px;
-    } 
+.aside {
+  width: 240px;
+}
 </style>
