47f8b9c9f5e9acc56ddf95acd42659d8a8164a63
qiangyanhuanxiao <5401587+chunchunge@user.noreply.gitee.com>
2019-11-12 19:41:27 +0800

    移除乘机人

--- components/air/orderForm.vue
+++ components/air/orderForm.vue
@@ -20,7 +20,7 @@
             </el-input>
           </el-form-item>
 
-          <span class="delete-user" @click="handleDeleteUser()">-</span>
+          <span class="delete-user" @click="handleDeleteUser(index)">-</span>
         </div>
       </el-form>
 
@@ -87,7 +87,9 @@ export default {
     },
 
     // 移除乘机人
-    handleDeleteUser() {},
+    handleDeleteUser(index) {
+      this.users.splice(index, 1);
+    },
 
     // 发送手机验证码
     handleSendCaptcha() {},
