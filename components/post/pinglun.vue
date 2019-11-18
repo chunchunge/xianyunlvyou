31c461d5ccafe9f55da16d597dbf2396ebe06fed
qiangyanhuanxiao <5401587+chunchunge@user.noreply.gitee.com>
2019-11-18 21:22:43 +0800

    创建评论组件

---
+++ components/post/pinglun.vue
@@ -0,0 +1,152 @@
+<template>
+  <div class="cmt-wrapper">
+    <h4 class="cmt-title">评论</h4>
+    <div class="cmt-input">
+      <div class="el-textarea">
+        <textarea
+          autocomplete="off"
+          placeholder="说点什么吧..."
+          class="el-textarea__inner"
+          style="resize: none; min-height: 33px;"
+        ></textarea>
+      </div>
+    </div>
+    <div class="cmt-tu">
+      <div class="tu">
+        <!-- 添加图片的组件 -->
+        <el-upload
+          action="http:127.0.0.1:1337/upload"
+          list-type="picture-card"
+          :on-preview="handlePictureCardPreview"
+          :on-remove="handleRemove"
+        >
+          <i class="el-icon-plus"></i>
+        </el-upload>
+        <el-dialog :visible.sync="dialogVisible">
+          <img width="100%" :src="dialogImageUrl" alt />
+        </el-dialog>
+      </div>
+      <div class="btn">
+        <el-row>
+          <el-button type="primary">提交</el-button>
+        </el-row>
+      </div>
+    </div>
+    <div class="cmt-list">
+      <div class="cmt-item">
+        <div class="cmt-info">
+          <div class="img"></div>
+          <span class="one">保护敌方水晶</span>
+          <i>2019-11-18 7:55</i>
+          <span class="louceng">1</span>
+        </div>
+        <div class="el-row">
+          <p></p>
+          <div class="cmt-pic">
+            <img src="../../assets/images/pic_sea.jpeg" alt style="width:78px;height:78px" />
+          </div>
+          <div class="cmt-ctrl">
+            <a href="javascript:;">回复</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      dialogImageUrl: "",
+      dialogVisible: false
+    };
+  },
+  methods: {
+    handleRemove(file, fileList) {
+      console.log(file, fileList);
+    },
+    handlePictureCardPreview(file) {
+      this.dialogImageUrl = file.url;
+      this.dialogVisible = true;
+    }
+  }
+};
+</script>
+
+<style lang="less" scoped>
+.cmt-wrapper {
+  margin-bottom: 20px;
+  .cmt-title {
+    font-weight: 400;
+    font-size: 18px;
+    margin-bottom: 20px;
+  }
+  .cmt-input {
+    margin-bottom: 10px;
+  }
+  .cmt-tu {
+    display: flex;
+    justify-content: space-between;
+  }
+  .cmt-list {
+    border: 1px solid #ddd;
+    height: 358px;
+    margin-top: 20px;
+    .cmt-item {
+      border-bottom: 1px dashed #ddd;
+      padding: 20px 20px 5px;
+      .cmt-info {
+        margin-bottom: 10px;
+        font-size: 12px;
+        color: #666;
+        position: relative;
+        .one {
+          margin-left: 20px;
+        }
+        .img {
+          width: 16px;
+          height: 16px;
+          border-radius: 50%;
+          background: skyblue;
+          position: absolute;
+          top: 0;
+          left: 0;
+        }
+        .louceng {
+          float: right;
+        }
+        .el-row {
+          width: 80px;
+          height: 80px;
+          box-sizing: border-box;
+          
+          
+        }
+      }
+    }
+  }
+}
+.cmt-ctrl {
+            height: 20px;
+            line-height: 20px;
+            font-size: 12px;
+            color: #1e50a2;
+            text-align: right;
+          }
+.el-upload {
+  width: 100px;
+  height: 100px;
+}
+.cmt-pic {
+            width: 80px;
+            height: 80px;
+            border-radius: 6px;
+            overflow: hidden;
+            margin-right: 5px;
+            margin-top: 10px;
+            padding: 5px;
+            border: 1px dashed #ddd;
+            margin-left:35px
+          }
+</style>
--- pages/post/xiangqing.vue
+++ pages/post/xiangqing.vue
@@ -5,33 +5,69 @@
         <el-breadcrumb-item :to="{ path: '/post' }">旅游攻略</el-breadcrumb-item>
         <el-breadcrumb-item>攻略详情</el-breadcrumb-item>
       </el-breadcrumb>
+      <div class="title">
+        <h1>{{data.title}}</h1>
+        <div class="post-info">
+          <span>攻略：2019-05-22 10:57</span>
+          <span>阅读：10090</span>
+        </div>
+      </div>
+      <div class="main" v-html="data.content"></div>
+      <div class="post-ctrl">
+        <div class="el-row is-justify-center el-row--flex">
+          <div class="ctrl-item">
+            <i class="iconfont iconpinglun"></i>
+            <p>评论(100)</p>
+          </div>
+          <div class="ctrl-item">
+            <i class="iconfont iconstar1"></i>
+            <p>收藏</p>
+          </div>
+          <div class="ctrl-item">
+            <i class="iconfont iconfenxiang"></i>
+            <p>分享</p>
+          </div>
+          <div class="ctrl-item">
+            <i class="iconfont iconding"></i>
+            <p> 
+              <span  v-if="data.like==null">点赞 0 </span>
+              <span  v-else>点赞 {{data.like}}</span>
+              </p>
+          </div>
+        </div>
+      </div>
+      <Pinglun/>
     </div>
-    <div class="right">
-      123
-    </div>
-  
+    <div class="right">123</div>
   </section>
 </template>
 
 <script>
+import Pinglun from "@/components/post/pinglun"
 export default {
-  mounted(){
+  components:{
+    Pinglun
+  },
+  data() {
+    return {
+      data: {}
+    };
+  },
+  mounted() {
     // 首先解构query带来的参数
     const { query } = this.$route;
     this.$axios({
-       url: `posts`,
-       params: {
-         id:query.id
-       }
-    }).then(res=>{
-      const {data} =res.data;
-      console.log(res);
-      console.log(query.id);
-      
-      
-    })
-
-    
+      url: `posts`,
+      params: {
+        id: query.id
+      }
+    }).then(res => {
+      const { data } = res.data;
+      this.data = data[0];
+      console.log(this.data);
+      console.log(this.data.content);
+      var main = document.querySelector(".main");
+    });
   }
 };
 </script>
@@ -42,11 +78,50 @@ export default {
   margin: 20px auto;
   display: flex;
   justify-content: space-between;
-  .left{
+  .left {
     width: 700px;
+    .title {
+      h1 {
+        padding: 15px 0;
+        border-bottom: 1px solid #eee;
+      }
+      .post-info {
+        color: #999;
+        padding: 20px;
+        text-align: right;
+      }
+    }
+    
+    .post-ctrl {
+      padding: 50px 0 30px;
+      .ctrl-item {
+        margin: 0 20px;
+        font-size: 20px;
+        text-align: center;
+        cursor: pointer;
+        .iconfont {
+          display: block;
+          font-size: 28px;
+          color: orange;
+        }
+        > p {
+          margin-top: 5px;
+          font-size: 14px;
+          color: #999;
+        }
+      }
+    }
+    
   }
-  .right{
+  
+  .right {
     width: 280px;
   }
 }
+// 深度选择器
+    .main /deep/ span > img {
+      width: 100%;
+      height: 100%;
+      padding: 10px 0;
+    }
 </style>
